9545f098a724bd1d4bc1f7d2af8914793c62ce0f
Matthew Ho <matthew.annam.ho@gmail.com>
2024-06-03 16:54:32 +0200

    adding setup.py configuration to make this pip installable

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup()
+setup(
+    name='tarp',
+    version='0.1.1',
+    author='Pablo Lemos',
+    url='https://github.com/Ciela-Institute/tarp',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+)
